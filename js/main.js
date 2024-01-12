6fda771e926386ef9ce8f5d87ab3d89d160c839d
Mbueno992 <mbueno_92@hotmail.com>
2024-01-12 11:31:02 +0100

    function check

--- js/main.js
+++ js/main.js
@@ -7,33 +7,39 @@ const btnAdd = document.querySelector('.js-btnAdd');
 const list = document.querySelector('.js-list');
 
 const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-  ];
+  { name: 'Recoger setas en el campo', completed: true },
+  { name: 'Comprar pilas', completed: true },
+  { name: 'Poner una lavadora de blancos', completed: true },
+  {
+    name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+    completed: false,
+  },
+];
 
-function check (event) {
-    const taskCheck = event.target.value;
-    console.log(taskCheck);
-    if( taskCheck === true) {
-        console.log('true')
-        list.classList.add('checked');
+// const taskCompleted = tasks.filter((task) => task.completed);
+// console.log(taskCompleted);
+
+function check(event) {
+  const taskCheck = event.target;
+  const taskCheckParent = taskCheck.parentNode;
+  for (const taskTrue of tasks) {
+    if (taskCheck.checked === true) {
+      taskCheckParent.classList.add('checked');
+      taskTrue.completed = true;
+    } else {
+      taskCheckParent.classList.remove('checked');
+      taskTrue.completed = false;
     }
+  }
+  console.log(tasks);
 }
 
 list.addEventListener('click', check);
 
-function renderTask (array) {
-    for( let i=0; i<array.length; i++) {
-       list.innerHTML += `<label><input type="checkbox" >${array[i].name}</label>`;
-    }
-   
+function renderTask(array) {
+  for (let i = 0; i < array.length; i++) {
+    list.innerHTML += `<li><input type="checkbox">${array[i].name}</li>`;
+  }
 }
 
 renderTask(tasks);
-
-
