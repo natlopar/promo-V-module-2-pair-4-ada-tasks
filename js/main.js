f9f5b4542bdd5590af7852f4bcbbe589bed0385d
Mbueno992 <mbueno_92@hotmail.com>
2024-01-15 11:30:19 +0100

    peticiones al servidor

--- js/main.js
+++ js/main.js
@@ -6,40 +6,63 @@ const btnSearch = document.querySelector('.js-btnSearch');
 const btnAdd = document.querySelector('.js-btnAdd');
 const list = document.querySelector('.js-list');
 
-const tasks = [
-  { name: 'Recoger setas en el campo', completed: true },
-  { name: 'Comprar pilas', completed: true },
-  { name: 'Poner una lavadora de blancos', completed: true },
-  {
-    name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-    completed: false,
-  },
-];
+// const tasks = [
+//   { name: 'Recoger setas en el campo', completed: true },
+//   { name: 'Comprar pilas', completed: true },
+//   { name: 'Poner una lavadora de blancos', completed: true },
+//   {
+//     name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+//     completed: false,
+//   },
+// ];
+let tasks = [];
+function renderTask(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].completed) {
+      list.innerHTML += `<li class="checked"><input type="checkbox" checked>${array[i].name}</li>`;
+    } else {
+      list.innerHTML += `<li><input type="checkbox">${array[i].name}</li>`;
+    }
+  }
+}
+function chargeData() {
+  fetch('https://dev.adalab.es/api/todo')
+    .then((response) => response.json())
+    .then((data) => {
+      for (const result of data.results) {
+        tasks = result;
+      }
+    });
+  renderTask(tasks);
+}
 
-// const taskCompleted = tasks.filter((task) => task.completed);
-// console.log(taskCompleted);
+function handleSearch(event) {
+  event.preventDefault();
+  const search = inputSearch.value;
+  const searchTask = tasks.filter((task) => {
+    if (task.name.includes(search)) {
+      return task.name;
+    }
+  });
+  list.innerHTML = '';
+  renderTask(searchTask);
+}
 
+btnSearch.addEventListener('click', handleSearch);
 function check(event) {
   const taskCheck = event.target;
   const taskCheckParent = taskCheck.parentNode;
-  for (const taskTrue of tasks) {
-    if (taskCheck.checked === true) {
-      taskCheckParent.classList.add('checked');
-      taskTrue.completed = true;
-    } else {
-      taskCheckParent.classList.remove('checked');
-      taskTrue.completed = false;
-    }
+  // Encuentra la tarea correspondiente en el array tasks
+  const task = tasks.find((item) => item.name === taskCheckParent.textContent); //busca el primer elemento de tasks, cuya propiedad name coincida con el contenido de texto del elemento padre del checkbox
+  // Verifica si el checkbox está marcado y actualiza la tarea correspondiente
+  if (taskCheck.checked) {
+    taskCheckParent.classList.add('checked');
+    task.completed = true;
+  } else {
+    taskCheckParent.classList.remove('checked');
+    task.completed = false;
   }
+
   console.log(tasks);
 }
-
 list.addEventListener('click', check);
-
-function renderTask(array) {
-  for (let i = 0; i < array.length; i++) {
-    list.innerHTML += `<li><input type="checkbox">${array[i].name}</li>`;
-  }
-}
-
-renderTask(tasks);
